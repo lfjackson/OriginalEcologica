e70f728d12abad6b2c3ae7e6641ddbe6ffbcf8a6
lfjackson <lfjackson@163.com>
2016-06-21 16:03:36 +0800

    第一次提交

---
+++ OriginalEcologica/APPDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OriginalEcologica
+//
+//  Created by 刘峰 on 16/6/21.
+//  Copyright © 2016年 刘峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OriginalEcologica/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OriginalEcologica
+//
+//  Created by 刘峰 on 16/6/21.
+//  Copyright © 2016年 刘峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
